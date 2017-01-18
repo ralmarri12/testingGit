9edf99911bfe63fe7f9ac3ace91bce2dba57c3ce
Rashid F <r.almarri12@gmail.com>
2017-01-18 12:38:53 +0800

    Htmlizeation

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Testing GIT - Homepage</title>
+    </head>
+    <body>
 
-echo 'Hello this is my index';
-
-?>
+        <h1>Hello</h1>
+        <p>This is my frist git reposotory</p>
+    </body>
+</html>
